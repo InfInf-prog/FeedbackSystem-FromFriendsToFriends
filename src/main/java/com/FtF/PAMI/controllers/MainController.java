b5a79959a5e2fcf918c75e1ff7e68c010b1c69b3
Polina <polina.brigadirenko@mail.ru>
2021-05-07 21:02:49 +0300

    changed resources to make Main page work

--- src/main/java/com/FtF/PAMI/controllers/MainController.java
+++ src/main/java/com/FtF/PAMI/controllers/MainController.java
@@ -1,16 +1,13 @@
 package com.FtF.PAMI.controllers;
 
 import org.springframework.stereotype.Controller;
-        import org.springframework.ui.Model;
-        import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class MainController {
 
     @GetMapping("/")
-    public String main(Model model) {
-        model.addAttribute("greeting", "Приветсвуем Вас в крутом приложении PAMI");
+    public String main() {
         return "main";
     }
-
 }
